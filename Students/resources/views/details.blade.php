c8364aa87fb300b93b646ab72f9d32e963b90f09
Mehedi Hasan <email.mehedi.bd@gmail.com>
2022-10-08 13:38:31 +0600

    crud operation done

--- Students/resources/views/details.blade.php
+++ Students/resources/views/details.blade.php
@@ -7,7 +7,24 @@
     <h4>Name: {{$all_info->name}}</h4>
     <h4>Birth Date: {{$all_info->birth_date}}</h4>
     <h4>Gender: {{$all_info->gender}}</h4>
-    <h4>Hobbies: {{$all_info->name}}</h4>
+    <h4>Hobbies:
+        <?php
+        if ($all_info->hobby1 != null) {
+            echo $all_info->hobby1 . ", ";
+        }
+        if ($all_info->hobby2 != null) {
+            echo  $all_info->hobby2 . ", ";
+        }
+        if ($all_info->hobby3 != null) {
+            echo $all_info->hobby3 . ", ";
+        }
+
+
+        ?>
+
+
+
+    </h4>
     <h4>Nationality: {{$all_info->nationality}}</h4>
 
     <a href="{{ route('student.update', $all_info->id) }}"><button type="button"
