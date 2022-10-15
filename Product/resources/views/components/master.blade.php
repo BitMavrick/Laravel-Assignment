e03e9871e134ab574dfd749eca8e2af652f8e075
Mehedi Hasan <email.mehedi.bd@gmail.com>
2022-10-15 15:16:32 +0600

    Finalize template

--- Product/resources/views/components/master.blade.php
+++ Product/resources/views/components/master.blade.php
@@ -17,27 +17,18 @@
     <link href="https://cdn.materialdesignicons.com/4.4.95/css/materialdesignicons.min.css" rel="stylesheet" />
 
     <!-- PLUGINS CSS STYLE -->
-    <link href="assets/plugins/simplebar/simplebar.css" rel="stylesheet" />
-    <link href="assets/plugins/nprogress/nprogress.css" rel="stylesheet" />
+    <link href="/assets/plugins/simplebar/simplebar.css" rel="stylesheet" />
+    <link href="/assets/plugins/nprogress/nprogress.css" rel="stylesheet" />
 
     <!-- No Extra plugin used -->
-    <link href='assets/plugins/jvectormap/jquery-jvectormap-2.0.3.css' rel='stylesheet'>
-    <link href='assets/plugins/daterangepicker/daterangepicker.css' rel='stylesheet'>
-
-
-    <link href='assets/plugins/toastr/toastr.min.css' rel='stylesheet'>
-
-
-
-
-
-
+    <link href='/assets/plugins/jvectormap/jquery-jvectormap-2.0.3.css' rel='stylesheet'>
+    <link href='/assets/plugins/daterangepicker/daterangepicker.css' rel='stylesheet'>
 
     <!-- SLEEK CSS -->
-    <link id="sleek-css" rel="stylesheet" href="assets/css/sleek.css" />
+    <link id="sleek-css" rel="stylesheet" href="/assets/css/sleek.css" />
 
     <!-- FAVICON -->
-    <link href="assets/img/favicon.png" rel="shortcut icon" />
+    <link href="/assets/img/favicon.png" rel="shortcut icon" />
 
     <!--
       HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries
@@ -47,7 +38,7 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-    <script src="assets/plugins/nprogress/nprogress.js"></script>
+    <script src="/assets/plugins/nprogress/nprogress.js"></script>
 </head>
 
 <body class="header-fixed sidebar-fixed sidebar-dark header-light" id="body">
@@ -768,8 +759,8 @@
                                                         <a href="javscript:void(0)"
                                                             class="media media-message media-notification media-active">
                                                             <div class="position-relative mr-3">
-                                                                <img class="rounded-circle" src="assets/img/user/u1.jpg"
-                                                                    alt="Image">
+                                                                <img class="rounded-circle"
+                                                                    src="/assets/img/user/u1.jpg" alt="Image">
                                                                 <span class="status active"></span>
                                                             </div>
                                                             <div class="media-body d-flex justify-content-between">
@@ -793,8 +784,8 @@
                                                         <a href="javscript:void(0)"
                                                             class="media media-message media-notification">
                                                             <div class="position-relative mr-3">
-                                                                <img class="rounded-circle" src="assets/img/user/u5.jpg"
-                                                                    alt="Image">
+                                                                <img class="rounded-circle"
+                                                                    src="/assets/img/user/u5.jpg" alt="Image">
                                                                 <span class="status away"></span>
                                                             </div>
                                                             <div class="media-body d-flex justify-content-between">
@@ -960,8 +951,8 @@
                                                         <a href="javscript:void(0)"
                                                             class="media media-message media-notification">
                                                             <div class="position-relative mr-3">
-                                                                <img class="rounded-circle" src="assets/img/user/u6.jpg"
-                                                                    alt="Image">
+                                                                <img class="rounded-circle"
+                                                                    src="/assets/img/user/u6.jpg" alt="Image">
                                                                 <span class="status away"></span>
                                                             </div>
                                                             <div class="media-body d-flex justify-content-between">
@@ -985,8 +976,8 @@
                                                         <a href="javscript:void(0)"
                                                             class="media media-message media-notification">
                                                             <div class="position-relative mr-3">
-                                                                <img class="rounded-circle" src="assets/img/user/u7.jpg"
-                                                                    alt="Image">
+                                                                <img class="rounded-circle"
+                                                                    src="/assets/img/user/u7.jpg" alt="Image">
                                                                 <span class="status away"></span>
                                                             </div>
                                                             <div class="media-body d-flex justify-content-between">
@@ -1010,8 +1001,8 @@
                                                         <a href="javscript:void(0)"
                                                             class="media media-message media-notification media-active">
                                                             <div class="position-relative mr-3">
-                                                                <img class="rounded-circle" src="assets/img/user/u1.jpg"
-                                                                    alt="Image">
+                                                                <img class="rounded-circle"
+                                                                    src="/assets/img/user/u1.jpg" alt="Image">
                                                                 <span class="status active"></span>
                                                             </div>
                                                             <div class="media-body d-flex justify-content-between">
@@ -1035,8 +1026,8 @@
                                                         <a href="javscript:void(0)"
                                                             class="media media-message media-notification">
                                                             <div class="position-relative mr-3">
-                                                                <img class="rounded-circle" src="assets/img/user/u2.jpg"
-                                                                    alt="Image">
+                                                                <img class="rounded-circle"
+                                                                    src="/assets/img/user/u2.jpg" alt="Image">
                                                                 <span class="status away"></span>
                                                             </div>
                                                             <div class="media-body d-flex justify-content-between">
@@ -1060,8 +1051,8 @@
                                                         <a href="javscript:void(0)"
                                                             class="media media-message media-notification">
                                                             <div class="position-relative mr-3">
-                                                                <img class="rounded-circle" src="assets/img/user/u5.jpg"
-                                                                    alt="Image">
+                                                                <img class="rounded-circle"
+                                                                    src="/assets/img/user/u5.jpg" alt="Image">
                                                                 <span class="status away"></span>
                                                             </div>
                                                             <div class="media-body d-flex justify-content-between">
@@ -1182,7 +1173,7 @@
 
                                                             <div class="media-body d-flex justify-content-between">
                                                                 <div class="message-contents">
-                                                                    <h4 class="title">Server overloaded</h4>
+                                                                    <h4 class="title">Server overloaded</h4>/
                                                                     <p class="last-msg font-size-14">Donec mattis augue
                                                                         a nisl consequat, nec imperdiet ex rutrum. Fusce
                                                                         et vehicula enim. Sed in enim eu odio vehic.</p>
@@ -1276,13 +1267,13 @@
                             <!-- User Account -->
                             <li class="dropdown user-menu">
                                 <button href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">
-                                    <img src="assets/img/user/user.png" class="user-image" alt="User Image" />
+                                    <img src="/assets/img/user/user.png" class="user-image" alt="User Image" />
                                     <span class="d-none d-lg-inline-block">Abdus Salam</span>
                                 </button>
                                 <ul class="dropdown-menu dropdown-menu-right">
                                     <!-- User image -->
                                     <li class="dropdown-header">
-                                        <img src="assets/img/user/user.png" class="img-circle" alt="User Image" />
+                                        <img src="/assets/img/user/user.png" class="img-circle" alt="User Image" />
                                         <div class="d-inline-block">
                                             Abdus Salam <small class="pt-1">iamabdus@gmail.com</small>
                                         </div>
@@ -1353,29 +1344,28 @@
     </script> -->
 
     <!-- Javascript -->
-    <script src="assets/plugins/jquery/jquery.min.js"></script>
-    <script src="assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="assets/plugins/simplebar/simplebar.min.js"></script>
+    <script src="/assets/plugins/jquery/jquery.min.js"></script>
+    <script src="/assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="/assets/plugins/simplebar/simplebar.min.js"></script>
 
-    <script src='assets/plugins/charts/Chart.min.js'></script>
-    <script src='assets/js/chart.js'></script>
-
-
+    <script src='/assets/plugins/charts/Chart.min.js'></script>
+    <script src='/assets/js/chart.js'></script>
 
 
-    <script src='assets/plugins/jvectormap/jquery-jvectormap-2.0.3.min.js'></script>
-    <script src='assets/plugins/jvectormap/jquery-jvectormap-world-mill.js'></script>
-    <script src='assets/js/vector-map.js'></script>
 
-    <script src='assets/plugins/daterangepicker/moment.min.js'></script>
-    <script src='assets/plugins/daterangepicker/daterangepicker.js'></script>
-    <script src='assets/js/date-range.js'></script>
 
-    <script src='assets/plugins/toastr/toastr.min.js'></script>
+    <script src='/assets/plugins/jvectormap/jquery-jvectormap-2.0.3.min.js'></script>
+    <script src='/assets/plugins/jvectormap/jquery-jvectormap-world-mill.js'></script>
+    <script src='/assets/js/vector-map.js'></script>
 
-    <script src="assets/js/sleek.js"></script>
-    <link href="assets/options/optionswitch.css" rel="stylesheet">
-    <script src="assets/options/optionswitcher.js"></script>
+    <script src='/assets/plugins/daterangepicker/moment.min.js'></script>
+    <script src='/assets/plugins/daterangepicker/daterangepicker.js'></script>
+    <script src='/assets/js/date-range.js'></script>
+
+    <script src='/assets/plugins/toastr/toastr.min.js'></script>
+
+    <script src="/assets/js/sleek.js"></script>
+    <link href="/assets/options/optionswitch.css" rel="stylesheet">
 </body>
 
 </html>
