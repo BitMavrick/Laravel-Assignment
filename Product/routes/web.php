c62965a673854dcf1474e1a53074bbb8ffc9eb85
Mehedi Hasan <email.mehedi.bd@gmail.com>
2022-10-15 17:23:52 +0600

    Brand Crud Done

--- Product/routes/web.php
+++ Product/routes/web.php
@@ -9,4 +9,7 @@ Route::prefix('brand')->group(function () {
     Route::get('/', [brandController::class, 'brand'])->name('brand');
     Route::get('/new', [brandController::class, 'create'])->name('brand.create');
     Route::post('/store', [brandController::class, 'store'])->name('brand.store');
+    Route::put('/update', [brandController::class, 'edit'])->name('brand.edit');
+    Route::get('/update/{id}', [brandController::class, 'update'])->name('brand.update');
+    Route::get('/delete/{id}', [brandController::class, 'delete'])->where('id', '[0-9]+')->name('brand.delete');
 });
